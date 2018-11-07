fa04e5ba8147c08a2d9e7bb4b8e4a5149023bbcf
aldebap <aldeba_bla@yahoo.com>
2018-11-07 02:04:30 -0200

    Adicao da contagem de bytes e linhas

--- wc/src/wc.py
+++ wc/src/wc.py
@@ -6,24 +6,48 @@
 #	Python version of Linux wc utility
 ################################################################################
 
-import  argparse
+import argparse
+
+#	function to make count on input file
+
+
+def countFromFile(_fileHandler):
+    byteCount = 0
+    lineCount = 0
+
+    while True:
+        byte = _fileHandler.read(1)
+        if '' == byte:
+            break
+
+        byteCount = byteCount + 1
+        if '\n' == byte:
+            lineCount = lineCount + 1
+
+    print format(lineCount) + ' ' + format(byteCount)
+
 
 #	entry point
 
-if( __name__ == '__main__' ):
+if(__name__ == '__main__'):
 
     #	parse command line interface arguments
-    parser = argparse.ArgumentParser( description = 'A Phyton implementation of Linux wc utility' )
+    parser = argparse.ArgumentParser(
+        description='A Phyton implementation of Linux wc utility')
 
-    parser.add_argument( '-c', '--bytes', dest='bytes', action='store_true', help='print the byte counts' )
-    parser.add_argument( '-m', '--chars', dest='chars', action='store_true', help='print the character counts' )
-    parser.add_argument( '-l', '--lines', dest='lines', action='store_true', help='print the newline counts' )
-    parser.add_argument( '-w', '--words', dest='words', action='store_true', help='print the word counts' )
+    parser.add_argument('-c', '--bytes', dest='bytes',
+                        action='store_true', help='print the byte counts')
+    parser.add_argument('-m', '--chars', dest='chars',
+                        action='store_true', help='print the character counts')
+    parser.add_argument('-l', '--lines', dest='lines',
+                        action='store_true', help='print the newline counts')
+    parser.add_argument('-w', '--words', dest='words',
+                        action='store_true', help='print the word counts')
 
     args = parser.parse_args()
 
     #   read the input file and make the summarizations
-    with open( 'somefile.txt', 'r' ) as inputFile:
-        for line in inputFile:
-
-    print '[WebServer] Starting the Web Server'
+    with open('/home/aldeba/.bashrc', 'r') as inputFile:
+        countFromFile(inputFile)
+        inputFile.close()
+        print ' FileName\n'
