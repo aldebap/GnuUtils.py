c4abc3f160ce04931c25a9548d69d84fc244f355
aldebap <aldeba_bla@yahoo.com>
2018-11-08 00:11:11 -0200

    Options to show only desired counts

--- wc/src/wc.py
+++ wc/src/wc.py
@@ -7,9 +7,13 @@
 ################################################################################
 
 import argparse
+import sys
+
+showByteCount = False
+showWordCount = False
+showLineCount = False
 
 #	function to make count on input file
-
 
 def countFromFile(_fileHandler, _fileName):
     byteCount = 0
@@ -33,12 +37,18 @@ def countFromFile(_fileHandler, _fileName):
         if '\n' == byte:
             lineCount = lineCount + 1
 
+    result = ' '
+    if True == showLineCount:
+        result = result + format(lineCount) + ' '
+    if True == showWordCount:
+        result = result + format(wordCount) + ' '
+    if True == showByteCount:
+        result = result + format(byteCount) + ' '
+
     if '' == _fileName:
-        print format(lineCount) + ' ' + format(wordCount) + \
-            ' ' + format(byteCount)
+        print result
     else:
-        print format(lineCount) + ' ' + format(wordCount) + \
-            ' ' + format(byteCount) + ' ' + _fileName
+        print result + _fileName
 
 
 #	entry point
@@ -46,25 +56,33 @@ def countFromFile(_fileHandler, _fileName):
 if(__name__ == '__main__'):
 
     #	parse command line interface arguments
-    parser = argparse.ArgumentParser(
-        description='A Phyton implementation of Linux wc utility')
+    parser = argparse.ArgumentParser( description='A Phyton implementation of GNU wc utility')
 
-    parser.add_argument('-c', '--bytes', dest='bytes',
-                        action='store_true', help='print the byte counts')
-    parser.add_argument('-m', '--chars', dest='chars',
-                        action='store_true', help='print the character counts')
-    parser.add_argument('-l', '--lines', dest='lines',
-                        action='store_true', help='print the newline counts')
-    parser.add_argument('-w', '--words', dest='words',
-                        action='store_true', help='print the word counts')
+    parser.add_argument('-c', '--bytes', dest='bytes', action='store_true', help='print the byte counts')
+    parser.add_argument('-m', '--chars', dest='chars', action='store_true', help='print the character counts')
+    parser.add_argument('-l', '--lines', dest='lines', action='store_true', help='print the newline counts')
+    parser.add_argument('-w', '--words', dest='words', action='store_true', help='print the word counts')
 
     parser.add_argument(dest='fileNames', nargs='*')
 
     args = parser.parse_args()
 
+    #   set the global options
+    if True == args.bytes:
+        showByteCount = True
+    if True == args.words:
+        showWordCount = True
+    if True == args.lines:
+        showLineCount = True
+
+    if False == showByteCount and False == showWordCount and False == showLineCount:
+        showByteCount = True
+        showWordCount = True
+        showLineCount = True
+
     #   read the input file and make the summarizations
     if 0 == len(args.fileNames):
-        countFromFile(StandardError, '')
+        countFromFile(sys.stdin, '')
     else:
         for fileName in args.fileNames:
             with open(fileName, 'r') as inputFile:
